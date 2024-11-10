c55cbf9aa858380c833705a5abe710aa2d68fa30
Yosi <Yosi@YosiWebiks.com>
2024-11-10 17:37:17 +0200

    root state type and conditioal rendering on the links in the navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { RootState, useAppSelector } from "../redux/store";
 
 export default function Nav() {
+  const user = useAppSelector((state: RootState) => state.user);
+
   return (
     <div className="nav">
-      <NavLink to={"/login"}>Login</NavLink>
-      <NavLink to={"/register"}>Register</NavLink>
-      <NavLink to={"/votes"}>Votes</NavLink>
-      <NavLink to={"/statistics"}>Statistics</NavLink>
-      <button onClick={() => alert("Log out successfully")}>Logout</button>
+      {user.user ? (
+        <>
+          <NavLink to={"/votes"}>Votes</NavLink>
+          {user.user.isAdmin && (
+            <NavLink to={"/statistics"}>Statistics</NavLink>
+          )}
+          <button onClick={() => alert("Log out successfully")}>Logout</button>
+        </>
+      ) : (
+        <>
+          <NavLink to={"/login"}>Login</NavLink>
+          <NavLink to={"/register"}>Register</NavLink>
+        </>
+      )}
     </div>
   );
 }
